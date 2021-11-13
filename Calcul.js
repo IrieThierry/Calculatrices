fa542fac08f18158714815fcb051d3d504b6c831
IrieThierry <thierry-irie@hotmail.com>
2021-11-13 17:47:21 +0000

    Ajout de fonctionnalité pour ne pas faire suivre deux opérareurs

--- Calcul.js
+++ Calcul.js
@@ -7,8 +7,22 @@ console.log(tab2)*/
 
 
 let btns = document.querySelectorAll('.btn')
+let operator = []
 btns.forEach(function(item){
     item.addEventListener('click',function(){
+
+        if (item.classList.contains('operator')) {
+            operator.push(0)
+           let op = operator.length
+           if(op >= 2){
+                let littleValue = document.querySelector(".little-screen")
+               littleValue.value = littleValue.value.slice(0, -1);
+            }
+        }
+        else{
+            operator = []
+        }
+
         let screen1 = document.querySelector('.little-screen')
         let screen2 = document.querySelector('.screen-content')     
      
--- README.md
+++ README.md
@@ -2,3 +2,19 @@
 ## irie
 
 ![image de la calculatrice](https://github.com/IrieThierry/A7/blob/main/calc.PNG?raw=true)
+
+## Fonctionnalité qui permet de ne pas faire suivre deux opérateurs
+ ```js
+
+ if (item.classList.contains('operator')) {
+            operator.push(0)
+           let op = operator.length
+           if(op >= 2){
+                let littleValue = document.querySelector(".little-screen")
+               littleValue.value = littleValue.value.slice(0, -1);
+            }
+        }
+        else{
+            operator = []
+        }
+```
